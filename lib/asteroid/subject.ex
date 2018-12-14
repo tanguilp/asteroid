28452caaa94067dbd12c7a2bd39085c55c3ffc12
Tangui <29804907+tanguilp@users.noreply.github.com>
2018-12-14 18:57:53 +0300

    Asteroid.Subject => same as for Asteroid.Client

--- lib/asteroid/subject.ex
+++ lib/asteroid/subject.ex
@@ -21,7 +21,7 @@ defmodule Asteroid.Subject do
   the default attributes from the configuration file into the object.
   """
   @spec new_from_id(AttrRep.id(), Keyword.t())
-    :: {:ok, t()} | {:error, :client_not_found}
+    :: {:ok, t()} | {:error, :subject_not_found}
   def new_from_id(id, opts \\ [attrs_autoload: true]) when is_binary(id) do
     subject = %Asteroid.Subject{id: id}
 
@@ -53,5 +53,63 @@ defmodule Asteroid.Subject do
       {:error, error} ->
         {:error, error}
     end
+  end
+
+  @doc """
+  Fetches an attribute if not already loaded in the subject
+  """
+  @spec fetch_attribute(t(), AttrRep.attribute()) :: t()
+  def fetch_attribute(subject, attribute) do
+    if subject.attrs[attribute] == nil do
+      module = astrenv(:attribute_repositories)[:subject][:impl]
+      config = astrenv(:attribute_repositories)[:subject][:opts]
+
+      case module.get(subject.id, attribute, config) do
+        {:ok, value} ->
+          put_attribute(subject, attribute, value)
+
+        {:error, _} ->
+          subject
+      end
+    else
+      subject
+    end
+  end
+
+  @doc """
+  Puts an attribute in the subject, overriding any existing one
+  """
+  @spec put_attribute(t(), AttrRep.attribute(), AttrRep.value()) :: t()
+  def put_attribute(subject, attribute, value) do
+    %{subject | attrs: Map.put(subject.attrs, attribute, value)}
+  end
+
+  @doc """
+  Deletes an attribute from the subject
+  """
+  @spec delete_attribute(t(), AttrRep.attribute()) :: t()
+  def delete_attribute(subject, attribute) do
+    module = astrenv(:attribute_repositories)[:subject][:impl]
+    config = astrenv(:attribute_repositories)[:subject][:opts]
+
+    module.delete(subject.id, attribute, config)
+
+    %{subject | attrs: Map.delete(subject.attrs, attribute)}
+  end
+
+  @doc """
+  Persists the attributes of the subject in its repository and returns the
+  unmodified subject
+  """
+  @spec store(t()) :: t()
+  def store(subject) do
+    module = astrenv(:attribute_repositories)[:subject][:impl]
+    config = astrenv(:attribute_repositories)[:subject][:opts]
+
+    for {attribute, value} <- subject.attrs do
+      module.put(subject.id, attribute, value, config)
+    end
+
+    subject
   end
 end
