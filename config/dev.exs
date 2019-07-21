2508003482d1344fdb1fea1967159a41e6246681
Tangui <29804907+tanguilp@users.noreply.github.com>
2019-07-21 23:56:00 +0300

    Cleanup config files and deps

--- config/dev.exs
+++ config/dev.exs
@@ -76,6 +76,19 @@ config :mnesia,
 config :hammer,
   backend: {Hammer.Backend.ETS, [expiry_ms: 60_000 * 60 * 4, cleanup_interval_ms: 60_000 * 10]}
 
+# to use Riak uncomment and configure the following lines
+
+#config :pooler,
+#  pools: [
+#    [
+#      name: :riak,
+#      group: :riak,
+#      max_count: 10,
+#      init_count: 5,
+#      start_mfa: {Riak.Connection, :start_link, ['127.0.0.1', 8087]}
+#    ]
+#  ]
+
 ######################################################################
 ######################################################################
 ################## Asteroid configuration ############################
