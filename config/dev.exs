23d0db09e1770a77151e014764cc8adb71c061a9
Tangui <29804907+tanguilp@users.noreply.github.com>
2020-04-30 23:02:41 +0300

    Set response_mode in configuration

--- config/dev.exs
+++ config/dev.exs
@@ -275,6 +275,8 @@ config :asteroid, :oauth2_response_types_enabled, [
   :id_token
 ]
 
+config :asteroid, :oauth2_response_mode_policy, :oidc_only
+
 config :asteroid, :api_error_response_verbosity, :normal
 
 config :asteroid, :oauth2_scope_callback, &Asteroid.OAuth2.Scope.grant_for_flow/2
